6ab29b97a3d3316683f639b726a2b32eaf41e58e
dagyeong Jeong <jdk09140@gmail.com>
2024-06-17 16:13:02 +0900

    Feat : tailwind 설치

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p className="text-bold underline text-red-500">안녕</p>
+        <span className="text-orange-900">넌 잘 지냈니?</span>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
