1ccd6603a4188fa93b00dac145ee89dd605e3047
yoon-gang00 <yk1008@konkuk.ac.kr>
2024-06-19 07:44:15 -0700

    feat/로고누르면 main페이지로 이동하는 기능

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-// src/App.js
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar.js';
-import Home from './components/Home.js';
+import Main from './components/Main.js';  // 홈 페이지 컴포넌트 가져오기
 import FreeBoard from './components/FreeBoard.js';
 import AdoptionBoard from './components/AdoptionBoard.js';
 import QnABoard from './components/QnABoard.js';
 import JarangBoard from './components/JarangBoard.js';
 import RecommendPlace from './components/RecommendPlace.js';
 
-
 const App = () => {
   return (
     <Router>
       <Navbar />
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Main />} />  {/* 홈 경로를 메인 페이지로 설정 */}
         <Route path="/freeboard" element={<FreeBoard />} />
         <Route path="/adoption" element={<AdoptionBoard />} />
         <Route path="/qna" element={<QnABoard />} />
-        <Route path="/proud" element={<JarangBoard />} />
+        <Route path="/jarang" element={<JarangBoard />} />
         <Route path="/recommended" element={<RecommendPlace />} />
       </Routes>
     </Router>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,20 +1,18 @@
-// src/components/Navbar.js
 import React from 'react';
 import { Link } from 'react-router-dom';
-import './Navbar.css'; // CSS 파일을 사용해 스타일을 적용합니다.
+import './Navbar.css'; 
 
 const Navbar = () => {
   return (
     <nav className="navbar">
       <div className="navbar-brand">
-        멍멍냥냥
+        <Link to="/">멍멍냥냥</Link>
       </div>
       <div className="navbar-links">
-        <Link to="/">Home</Link>
         <Link to="/freeboard">자유 게시판</Link>
         <Link to="/adoption">입양 / 임시보호</Link>
         <Link to="/qna">고민 해결소</Link>
-        <Link to="/proud">자랑 게시판</Link>
+        <Link to="/jarang">자랑 게시판</Link>
         <Link to="/recommended">추천 Place</Link>
       </div>
       <div className="navbar-auth">
